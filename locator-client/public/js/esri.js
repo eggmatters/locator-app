6f8ac95cb37db46819a04711c6f0b8f73c991805
eggmatters <eggmatters@eggmatters.com>
2018-06-17 08:55:35 -0700

    Rendered arbitrary symbol point in map

--- locator-client/public/js/esri.js
+++ locator-client/public/js/esri.js
@@ -1,15 +1,41 @@
 require([
   "esri/Map",
-  "esri/views/MapView",
-  "dojo/domReady!"
-], function(Map, MapView){
+   "esri/views/MapView",
+   "esri/Graphic",
+   "dojo/domReady!"
+], function(Map, MapView, Graphic){
   var map = new Map({
     basemap: "streets"
   });
+
   var view = new MapView({
     container: "viewDiv",  // Reference to the scene div created in step 5
     map: map,  // Reference to the map object created before the scene
-    zoom: 4,  // Sets zoom level based on level of detail (LOD)
-    center: [15, 65]  // Sets center point of view using longitude,latitude
+    zoom: 15,  // Sets zoom level based on level of detail (LOD)
+    center: [-122.762, 45.416]  // Sets center point of view using longitude,latitude
   });
+
+  var point = {
+        type: "point", // autocasts as new Point()
+        longitude: -122.762,
+        latitude: 45.416
+   };
+
+   // Create a symbol for drawing the point
+   var markerSymbol = {
+      type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+      color: [40, 119, 226],
+      outline: {// autocasts as new SimpleLineSymbol()
+         color: [255, 255, 255],
+         width: 2
+      }
+   };
+
+   // Create a graphic and add the geometry and symbol to it
+   var pointGraphic = new Graphic({
+      geometry: point,
+      symbol: markerSymbol
+   });
+
+   view.graphics.addMany([pointGraphic]);
 });
