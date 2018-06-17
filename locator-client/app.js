444d0ed1068c0fc948f099fa33ccbf37444ad0ad
eggmatters <eggmatters@eggmatters.com>
2018-06-16 21:13:30 -0700

    skeleton express server

---
+++ locator-client/app.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, function(){
+  console.log('hello world');
+});
+
+app.get('/', function(req, res){
+    res.send('Hello Express');
+});
