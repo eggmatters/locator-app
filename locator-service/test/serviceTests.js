3522e6fd839eee3c5e49e981e50b853cd430dc95
eggmatters <eggmatters@eggmatters.com>
2018-06-21 07:59:48 -0700

    Added queuesTests

--- locator-service/test/serviceTests.js
+++ locator-service/test/serviceTests.js
@@ -1,8 +1,11 @@
 var expect     = require('chai').expect,
     assert     = require('chai').assert;
-    service    = require("../src/service");
+    service    = require("../src/service"),
+    sinon      = require('sinon'),
+    redis      = require('redis');
 
 describe ('Service Application', () => {
+
    it('Should fetch json from a request', () => {
       service.getRoutes(76).then( (res, err) => {
          return expect(res).to.not.be.empty;
