c1bd181f67a8ef884ceac523140bb2ff889d9078
eggmatters <eggmatters@eggmatters.com>
2018-06-29 20:31:47 -0700

    Initialized pub/sub redis queue, request initiation for service

--- locator-service/index.js
+++ locator-service/index.js
@@ -1,7 +1,37 @@
-var routes = require('./src/service');
+var routes = require('./src/service'),
+    queues = require('./src/queues'),
+    yaml   = require('js-yaml'),
+    fs     = require('fs');
+    http   = require('http');
 
-console.log("Starting locator-service");
-routes.getRoutes('76').then( (res, err) => {
-   console.log("Putting queue items");
-   routes.putQueueItems(res);
+
+const config = yaml.safeLoad(fs.readFileSync('./config/config.yml', 'utf8'));
+const client = (new queues()).getClient();
+
+const port = 8080;
+
+const requestHandler = (request, response) => {
+   let body = [];
+   request.on('data', (chunk) => {
+      console.log(chunk.toString());
+      body.push(chunk);
+   }).on('end', () => {
+      //body = Buffer.concat(body).toString();
+      console.log(body);
+      client.publish(config.redis.locations_queue, "Message is: GOT HERE");
+      // at this point, `body` has the entire request body stored in it as a string
+   });
+   response.end('Publishing to queue.');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
 });
+
+
